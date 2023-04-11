79e404d036920f8050e40a50a9d162c16f36025f
CesarTorrens <cesar.a.torrens@gmail.com>
2023-04-11 17:21:55 -0500

    add msg in home

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -3,22 +3,27 @@ import style from "./cards.module.css";
 
 export default function Cards({ characters, onClose }) {
   return (
-    <div className={style.container}>
-      {characters.map((character) => {
-        return (
-          <Card
-            key={character?.id}
-            id={character?.id}
-            name={character?.name}
-            image={character?.image}
-            gender={character?.gender}
-            origin={character?.origin}
-            species={character?.species}
-            status={character?.status}
-            onClose={onClose}
-          />
-        );
-      })}
-    </div>
+    <>
+      {characters.length < 1 && (
+        <h1 className={style.title}> Consulta tus personajes! </h1>
+      )}
+      <div className={style.container}>
+        {characters?.map((character) => {
+          return (
+            <Card
+              key={character?.id}
+              id={character?.id}
+              name={character?.name}
+              image={character?.image}
+              gender={character?.gender}
+              origin={character?.origin}
+              species={character?.species}
+              status={character?.status}
+              onClose={onClose}
+            />
+          );
+        })}
+      </div>
+    </>
   );
 }
